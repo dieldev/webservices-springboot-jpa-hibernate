83ae2367d5f332180190911660a048301c02ac7d
Sidiel Oliveira <sidiel.dev@gmail.com>
2023-08-07 15:44:20 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projetoweb/course/User.java
+++ src/main/java/com/projetoweb/course/User.java
@@ -2,14 +2,23 @@ package com.projetoweb.course;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L; 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
