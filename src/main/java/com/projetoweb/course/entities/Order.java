d7aa4159d7b58152ad093074002bfc6771684d2c
Sidiel Oliveira <sidiel.dev@gmail.com>
2023-08-14 19:02:16 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/projetoweb/course/entities/Order.java
+++ src/main/java/com/projetoweb/course/entities/Order.java
@@ -1,14 +1,14 @@
 package com.projetoweb.course.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,35 +16,27 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable {
-    private static final long serialVersionUID = 1L; 
-
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String email;
-    private String phone;
-    private String password;
+    private Instant moment;
 
-    @OneToMany(mappedBy = "client")
-    private List<Order> orders = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
 
-    public User() {
+    public Order() {
     }
 
-    public User(Long id, String name, String email, String phone, String password) {
+    public Order(Long id, Instant moment, User client) {
         super();
         this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-    }
-
-    public List<Order> getOrders() {
-        return orders;
+        this.moment = moment;
+        this.client = client;
     }
 
     @Override
@@ -63,12 +55,12 @@ public class User implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        User other = (User) obj;
+        Order other = (Order) obj;
         if (id == null) {
-            if (other.id != null)
+            if (other.id != null) 
                 return false;
         } else if (!id.equals(other.id))
             return false;
         return true;
-    }    
+        }
 }
